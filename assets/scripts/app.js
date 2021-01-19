2c528299232dc2fda8ddbfdac76c3dde58a7bdea
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-19 19:26:25 +0100

    using fetch()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,38 +5,40 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-	const promise = new Promise((resolve, reject) => {
-		const xhr =  new XMLHttpRequest();
+//	const promise = new Promise((resolve, reject) => {
+//		const xhr =  new XMLHttpRequest();
+//
+//		xhr.open(method, url);
+//		xhr.responseType = 'json';
 
-		xhr.open(method, url);
+//		xhr.onload = function() {
+//			if (xhr.status >= 200 && xhr.status < 300) {
+//				resolve(xhr.response);
+//			} else {
+//				reject(new Error('Something went wrong!'))
+//			}
+//			resolve(xhr.response);
+//			//const listOfPosts = JSON.parse(xhr.response);
+//		};
 
-		xhr.responseType = 'json';
+//		xhr.onerror =  function() {
+//			reject(new Error('Failed to send request'))
+//		}
 
-		xhr.onload = function() {
-			if (xhr.status >= 200 && xhr.status < 300) {
-				resolve(xhr.response);
-			} else {
-				reject(new Error('Something went wrong!'))
-			}
-			resolve(xhr.response);
-			//const listOfPosts = JSON.parse(xhr.response);
-		};
+//		xhr.send(JSON.stringify(data));
 
-		xhr.onerror =  function() {
-			reject(new Error('Failed to send request'))
-		}
-
-		xhr.send(JSON.stringify(data));
+//	});
+//	return promise;
+	return fetch(url).then(response => {
+		return response.json();
 	});
-	
-	return promise;
 }
 
 async function fetchPosts() {
-	try {
+//	try {
 		const responseData = await sendHttpRequest(
 		'GET', 
-		'https://jsonplaceholder.typicode.com/pos'
+		'https://jsonplaceholder.typicode.com/posts'
 		);
 		const listOfPosts = responseData;
 		for (const post of listOfPosts) {
@@ -46,9 +48,9 @@ async function fetchPosts() {
 			postEl.querySelector('li').id = post.id;
 			listElement.append(postEl);
 		}
-	} catch (error) {
-		alert(error.message);
-	}
+//	} catch (error) {
+//		alert(error.message);
+//	}
 	
 }
 
